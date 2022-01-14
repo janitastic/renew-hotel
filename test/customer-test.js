5f291bc869583dd2accc4527a64a63c9d6f4e2da
Jani Palomino <palomino.jani@gmail.com>
2022-01-13 23:37:55 -0700

    Complete Customer constructor and instantiating tests

--- test/customer-test.js
+++ test/customer-test.js
@@ -16,4 +16,29 @@ describe.only('Customer', () => {
   it('Should be a function', () => {
     expect(Customer).to.be.a('function');
   });
+
+  it('Should instantiate a new instance of a customer', () => {
+    expect(customer1).to.be.an.instanceOf(Customer);
+  });
+
+
+  it('Should have an id and a name', () => {
+    expect(customer1).to.have.a.property('id');
+    expect(customer1.id).to.equal(1);
+    expect(customer1).to.have.a.property('name');
+    expect(customer1.name).to.equal('Leatha Ullrich');
+  });
+
+  it('Should be able to instantiate a different customer', () => {
+    expect(customer2).to.be.an.instanceOf(Customer);
+    expect(customer2).to.have.a.property('id');
+    expect(customer2.id).to.equal(2);
+    expect(customer2).to.have.a.property('name');
+    expect(customer2.name).to.equal('Rocio Schuster');
+  });
+
+  it('Should start out with no bookings', () => {
+    expect(customer1).to.have.a.property('bookings');
+    expect(customer1.bookings).to.deep.equal([]);
+  });
 });
