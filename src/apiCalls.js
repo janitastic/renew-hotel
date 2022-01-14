ad699d38558e60e604106fda60101650edee6945
Jani Palomino <palomino.jani@gmail.com>
2022-01-13 22:15:18 -0700

    Load data in console

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,40 +1,47 @@
+import {loadPage} from './scripts.js';
 /*************** FETCH CALLS ***************/
 
 const fetchCustomers = () => {
-  fetch(customers) {
-    .then(response => response.json())
-    .then(data => response.json())
-    .catch(error => console.log(error))
-  }
-}
-
-const fetchBookings = () => {
-  fetch(bookings) {
+  fetch(customers)
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.log(error))
-  }
+}
+
+const fetchBookings = () => {
+  fetch(bookings)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+}
+
+/*************** PROMISE ***************/
+
+const fetchAllData = () => {
+  Promise.all([fetchCustomers(), fetchBookings()])
+    .then(data => console.log(data))
 }
 
 /*************** POST REQUESTS ***************/
 
 const bookRoom = (userId, selectedDate, roomNumber) => {
-  fetch(bookings, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(
-      { "userID": 48, "date": "2019/09/23", "roomNumber": 4 }
-    )
-  })
-  .then(response => {
-    console.log(response, '<<<<response>>>>')
-    // checkForErrors(response);//built out this function to look for the error
-    response.json()})//promise object that's returned to us. Carries the properties and values
-  .then(data => console.log(data))
-  .catch(error => showError(error));
+  fetch(url);
+  // fetch(bookings, {
+  //   method: 'POST',
+  //   headers: { 'Content-Type': 'application/json' },
+  //   body: JSON.stringify(
+  //     { "userID": 48, "date": "2019/09/23", "roomNumber": 4 }
+  //   )
+  // })
+  // .then(response => {
+  //   console.log(response, '<<<<response>>>>')
+  //   // checkForErrors(response);//built out this function to look for the error
+  //   response.json()})//promise object that's returned to us. Carries the properties and values
+  // .then(data => console.log(data))
+  // .catch(error => showError(error));
 }
 
-bookRoom(18, '2021/01/13', 5);
+// bookRoom(18, '2021/01/13', 5);
 
 
 /*************** ERROR HANDLING ***************/
@@ -43,8 +50,7 @@ bookRoom(18, '2021/01/13', 5);
 
 const customers = 'http://localhost:3001/api/v1/customers';
 const bookings = 'http://localhost:3001/api/v1/bookings';
-fetchCustomers();
 
 /*************** QUERY SELECTORS ***************/
 
-export default {fetchCustomers, fetchBookings};
+export default {fetchAllData, fetchCustomers, fetchBookings};
--- src/scripts.js
+++ src/scripts.js
@@ -3,9 +3,20 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import apiCalls from './apiCalls';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+/**************** EVENT LISTENERS ****************/
+
+window.addEventListener('load', apiCalls.fetchAllData);
+
+
+/**************** FUNCTIONS ****************/
+const loadPage = () => {
+  console.log('data received')
+}
