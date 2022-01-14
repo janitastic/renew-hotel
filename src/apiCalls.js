35bb108422db2a19576cf233c45fe9880f6aea6f
Jani Palomino <palomino.jani@gmail.com>
2022-01-13 21:24:15 -0700

    Setup fetch calls skeleton

---
+++ src/apiCalls.js
@@ -0,0 +1,50 @@
+/*************** FETCH CALLS ***************/
+
+const fetchCustomers = () => {
+  fetch(customers) {
+    .then(response => response.json())
+    .then(data => response.json())
+    .catch(error => console.log(error))
+  }
+}
+
+const fetchBookings = () => {
+  fetch(bookings) {
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+  }
+}
+
+/*************** POST REQUESTS ***************/
+
+const bookRoom = (userId, selectedDate, roomNumber) => {
+  fetch(bookings, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(
+      { "userID": 48, "date": "2019/09/23", "roomNumber": 4 }
+    )
+  })
+  .then(response => {
+    console.log(response, '<<<<response>>>>')
+    // checkForErrors(response);//built out this function to look for the error
+    response.json()})//promise object that's returned to us. Carries the properties and values
+  .then(data => console.log(data))
+  .catch(error => showError(error));
+}
+
+bookRoom(18, '2021/01/13', 5);
+
+
+/*************** ERROR HANDLING ***************/
+
+/*************** VARIABLES ***************/
+
+const customers = 'http://localhost:3001/api/v1/customers';
+const bookings = 'http://localhost:3001/api/v1/bookings';
+fetchCustomers();
+
+/*************** QUERY SELECTORS ***************/
+
+export default {fetchCustomers, fetchBookings};
