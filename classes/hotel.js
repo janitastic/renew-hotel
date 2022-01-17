d6862f6bb43077c76549c01966b0445278c66a08
Jani Palomino <palomino.jani@gmail.com>
2022-01-16 19:31:45 -0700

    Move room filter functions to hotel class

---
+++ classes/hotel.js
@@ -0,0 +1,58 @@
+import Room from './room';
+import Booking from './booking';
+
+class Hotel {
+  constructor(roomsData, bookingsData, customersData) {
+    this.rooms = roomsData;
+    this.bookings = bookingsData;
+    this.customers = customersData;
+    this.availableRooms = [];
+    this.roomTypes = [];
+  }
+
+  logRoomTypes() {
+    const allRoomTypes = this.rooms.map(room => room.roomType);
+    return this.roomTypes = [...new Set(allRoomTypes)];
+  }
+
+  filterRoomsByDate(selectedDate) {
+    const bookedRooms = this.bookings.reduce((available, booking) => {
+      if (booking.date === selectedDate) {
+        available.push(booking.roomNumber);
+      }
+      return available;
+    },[]);
+
+    const allRoomNumbers = this.rooms.map(room => room.number);
+    const removedRooms = new Set(bookedRooms);
+    const availableRoomNumbers = allRoomNumbers.filter(number => {
+      return !removedRooms.has(number);
+    });
+
+    this.availableRooms = this.rooms.reduce((roomDetails, room) => {
+      if (availableRoomNumbers.includes(room.number)) {
+        roomDetails.push(room)
+      }
+      return roomDetails;
+    }, []);
+    return this.availableRooms;
+  }
+
+  filterAllRoomsByType(selectedType) {
+    return this.rooms.filter(room => room.roomType === selectedType)
+  }
+
+  filterAvailableRoomsByType(selectedType) {
+    // this.filterRoomsByDate(selectedDate)
+    if (!this.availableRooms.length) {
+      this.availableRooms = this.rooms.filter(room => room.roomType === selectedType);
+      // console.log('available rooms', this.availableRooms);
+      // return 'no available rooms message';
+    } else {
+      this.availableRooms = this.availableRooms.filter(room => room.roomType === selectedType);
+      // console.log('available rooms 2', this.availableRooms);
+    }
+  }
+}
+
+export default Hotel;
--- test/hotel-test.js
+++ test/hotel-test.js
@@ -28,7 +28,7 @@ describe('Hotel', () => {
   it('Should have a rooms property that stores all the rooms in the hotel', () => {
     expect(hotel).to.have.a.property('rooms');
     expect(hotel.rooms).to.be.an('Array');
-    expect(hotel.rooms.length).to.deep.equal(6);
+    expect(hotel.rooms.length).to.deep.equal(25);
     expect(hotel.rooms).to.deep.equal(roomsData);
   });
 
@@ -49,5 +49,59 @@ describe('Hotel', () => {
   it('Should have an available rooms property that stores all the available rooms in the hotel', () => {
     expect(hotel).to.have.a.property('availableRooms');
     expect(hotel.availableRooms).to.deep.equal([]);
+  });
+
+  it('Should be able to log all room types with no duplicates', () => {
+    const roomTypesList = hotel.logRoomTypes();  
+
+    expect(roomTypesList).to.be.an('array');
+    expect(roomTypesList.length).to.deep.equal(4);
+    expect(roomTypesList).to.deep.equal([ 'residential suite', 'suite', 'single room', 'junior suite' ]);
+  });
+
+  it('Should be able to filter room availability by date', () => {
+    const selectedDate = '2022/01/10';
+
+    const availableRooms = hotel.filterRoomsByDate(selectedDate);  
+    // console.log(availableRooms);
+
+    expect(selectedDate).to.be.a.dateString();
+    expect(availableRooms).to.be.an('array');
+    expect(availableRooms.length).to.deep.equal(24);
+  });
+
+  it('Should be able to filter the list of rooms by their room type', () => {
+    const residential = 'residential suite';
+    const single = 'single room';
+
+    const residentialType = hotel.filterAllRoomsByType(residential);  
+    const singleType = hotel.filterAllRoomsByType(single);  
+
+    expect(residential).to.be.a('string');
+    expect(residentialType).to.be.an('array');
+    expect(residentialType.length).to.deep.equal(5);
+
+    expect(singleType).to.be.an('array');
+    expect(singleType.length).to.deep.equal(13);
+  });
+
+  it('Should be able to filter the list of available rooms by their roomType property', () => {
+    const selectedDate = '2022/01/10';
+    
+    const residential = 'residential suite';
+    const single = 'single room';
+    
+    hotel.filterRoomsByDate(selectedDate); 
+
+    const residentialType = hotel.filterAllRoomsByType(residential);
+    const singleType = hotel.filterAllRoomsByType(single);
+
+    expect(residential).to.be.a('string');
+    expect(residentialType).to.be.an('array');
+    expect(residentialType.length).to.deep.equal(5);
+
+    expect(single).to.be.a('string');
+    expect(singleType).to.be.an('array');
+    expect(singleType.length).to.deep.equal(13);
   });
 });
